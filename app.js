069e602e62dec60a41a10785e2f740b33e82c0c7
sajaalraja <>
2021-04-26 20:01:08 +0000

    updated

---
+++ app.js
@@ -0,0 +1,62 @@
+"use strict"
+alert("welcome");
+let nameq=prompt('whats your name owner the website saja ,esraa ?').toLowerCase();
+switch(nameq){
+  case 'saja':
+  alert("the answer is right");
+  console.log("right");
+  break;
+  default:
+  alert("the answer is wrong");
+  console.log("wrong");
+  break;
+
+}
+let myage=prompt('whats  age owner the website ?')
+switch(myage){
+  case '27':
+  alert("the answer is right");
+  console.log("right");
+  break;
+  default:
+  alert("the answer is wrong");
+  console.log("wrong");
+  break;
+}
+let major=prompt('whats  major study in the unieversity  owner the website ?').toLowerCase();
+switch(major){
+  case  'computer science':
+  alert("the answer is right");
+  console.log("right");
+  break;
+  default:
+  alert("the answer is wrong");
+  console.log("wrong");
+  break;
+}
+let year =prompt('when graduated from unieversity  owner the website ?')
+switch(year){
+  case  '2016':
+  alert("the answer is right");
+  console.log("right");
+  break;
+  default:
+  alert("the answer is wrong");
+  console.log("wrong");
+  break;
+}
+let job  =prompt('whats  job  before year worked  owner the website ?').toLowerCase();
+switch(job){
+  case  'teacher' :
+  console.log("right");
+  break;
+  default:
+  alert("the answer is wrong");
+  console.log("wrong");
+  break;
+}
+alert ( 'thankyou  for  answering question'   );
+
+
+
+
