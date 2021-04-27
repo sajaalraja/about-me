a33b485b26470542667819817b95b0bb156b3de2
sajaalraja <sajamajed611@yahoo.com>
2021-04-27 14:24:49 +0300

    updated

--- app.js
+++ app.js
@@ -1,58 +1,59 @@
 "use strict"
 alert("welcome");
-let nameq=prompt('whats your name owner the website saja ,esraa ?').toLowerCase();
+let nameq=prompt('do you name owner the website ?').toLowerCase();
 switch(nameq){
-  case 'saja':
-  alert("the answer is right");
-  console.log("right");
+  case 'yes':
+  alert("the answer is saja");
+  console.log("saja");
   break;
   default:
-  alert("the answer is wrong");
-  console.log("wrong");
+  alert("ok no problem ");
+  console.log("no problem ");
   break;
 
 }
-let myage=prompt('whats  age owner the website ?')
+let myage=prompt('do you  age owner the website ?').toLowerCase();
 switch(myage){
-  case '27':
-  alert("the answer is right");
-  console.log("right");
+  case 'yes':
+  alert("the answer is 27");
+  console.log("the answer is 27");
   break;
   default:
-  alert("the answer is wrong");
-  console.log("wrong");
+  alert("ok no problem");
+  console.log("no problem ");
   break;
 }
-let major=prompt('whats  major study in the unieversity  owner the website ?').toLowerCase();
+let major=prompt('do you   major study in the unieversity  owner the website ?').toLowerCase();
 switch(major){
-  case  'computer science':
-  alert("the answer is right");
-  console.log("right");
+  case  'yes':
+  alert("the answer is computer science ");
+  console.log("computer science ");
   break;
   default:
-  alert("the answer is wrong");
-  console.log("wrong");
+  alert("ok no problem ");
+  console.log("no problem ");
   break;
 }
-let year =prompt('when graduated from unieversity  owner the website ?')
+let year =prompt('do you know when  graduated from unieversity  owner the website ?')
 switch(year){
-  case  '2016':
-  alert("the answer is right");
-  console.log("right");
+  case  'yes':
+  alert("the answer is 2016");
+  console.log("2016");
   break;
   default:
-  alert("the answer is wrong");
-  console.log("wrong");
+  alert("ok no problem ");
+  console.log("no problem ");
   break;
 }
-let job  =prompt('whats  job  before year worked  owner the website ?').toLowerCase();
+let job  =prompt('do you know   job  before year worked  owner the website ?').toLowerCase();
 switch(job){
-  case  'teacher' :
-  console.log("right");
+  case  'yes' :
+    alert("the answer is teacher");
+  console.log("tachert");
   break;
   default:
-  alert("the answer is wrong");
-  console.log("wrong");
+  alert("ok no problem");
+  console.log("no problem");
   break;
 }
 alert ( 'thankyou  for  answering question'   );
